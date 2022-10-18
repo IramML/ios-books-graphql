03bbeb12a108c6f168ea4ce77423d4916b1eb7f0
IramML <irammartinezlamas@gmail.com>
2022-10-18 15:45:21 -0500

    Refactor datasource stub to spy

--- ApolloGraphQLSampleTests/Data/BooksRepositoryTests.swift
+++ ApolloGraphQLSampleTests/Data/BooksRepositoryTests.swift
@@ -11,7 +11,7 @@ import XCTest
 final class BooksRepositoryTests: XCTestCase {
     
     func test_getBooksFromRemote() {
-        let repository = createSpyRepositorySpy()
+        let (repository, dataSource) = makeSUT()
         
         let testExpectation = XCTestExpectation()
         var booksRetreived: [Book]? = nil
@@ -27,20 +27,25 @@ final class BooksRepositoryTests: XCTestCase {
             testExpectation.fulfill()
         }
         
+        let booksResponse = [anyBook(), anyBook()]
+        dataSource.performSuccessGetBooksCompletion(byIndex: 0, books: booksResponse)
+        
         wait(for: [testExpectation], timeout: 1.0)
         
         XCTAssertNotNil(booksRetreived)
         XCTAssertEqual(booksRetreived!.count, 2)
-        XCTAssertEqual(repository.receivedRequests, [.getBooks])
+        XCTAssertEqual(booksRetreived![0].id, booksResponse[0].id)
+        XCTAssertEqual(booksRetreived![1].id, booksResponse[1].id)
+        XCTAssertEqual(dataSource.receivedRequests, [.getBooks])
     }
     
     func test_getBookByIdFromRemote() {
-        let repository = createSpyRepositorySpy()
-        let bookId: String = UUID().uuidString
+        let (repository, dataSource) = makeSUT()
         
         let testExpectation = XCTestExpectation()
         var bookRetreived: Book? = nil
         
+        let bookId: String = UUID().uuidString
         repository.getBookFromRemote(bookId: bookId) { result in
             switch result {
             case let .success(book):
@@ -52,15 +57,18 @@ final class BooksRepositoryTests: XCTestCase {
             testExpectation.fulfill()
         }
         
+        let response: Book = anyBook(withId: bookId)
+        dataSource.performSuccessGetBookCompletion(byIndex: 0, book: response)
+        
         wait(for: [testExpectation], timeout: 1.0)
         
         XCTAssertNotNil(bookRetreived)
-        XCTAssertEqual(bookRetreived!.id, bookId)
-        XCTAssertEqual(repository.receivedRequests, [.getBook(bookId: bookId)])
+        XCTAssertEqual(bookRetreived!.id, response.id)
+        XCTAssertEqual(dataSource.receivedRequests, [.getBook(bookId: bookId)])
     }
     
     func test_createBookFromRemote() {
-        let repository = createSpyRepositorySpy()
+        let (repository, dataSource) = makeSUT()
         let book: Book = anyBook()
         
         let testExpectation = XCTestExpectation()
@@ -77,14 +85,17 @@ final class BooksRepositoryTests: XCTestCase {
             testExpectation.fulfill()
         }
         
+        let bookId = book.id
+        dataSource.performSuccessCreateBookCompletion(byIndex: 0, bookId: bookId)
+        
         wait(for: [testExpectation], timeout: 1.0)
         
         XCTAssertNotNil(bookIdRetreived)
-        XCTAssertEqual(repository.receivedRequests, [.createBook(book: book)])
+        XCTAssertEqual(dataSource.receivedRequests, [.createBook(book: book)])
     }
     
     func test_deleteBookFromRemote() {
-        let repository = createSpyRepositorySpy()
+        let (repository, dataSource) = makeSUT()
         let bookId: String = UUID().uuidString
         
         let testExpectation = XCTestExpectation()
@@ -100,15 +111,17 @@ final class BooksRepositoryTests: XCTestCase {
             }
             testExpectation.fulfill()
         }
-        
+        let response = true
+        dataSource.performSuccessDeleteBookCompletion(byIndex: 0, completed: response)
         wait(for: [testExpectation], timeout: 1.0)
         
         XCTAssertNotNil(didDelete)
-        XCTAssertEqual(repository.receivedRequests, [.deleteBook(bookId: bookId)])
+        XCTAssertEqual(didDelete, response)
+        XCTAssertEqual(dataSource.receivedRequests, [.deleteBook(bookId: bookId)])
     }
     
     func test_updateBookFromRemote() {
-        let repository = createSpyRepositorySpy()
+        let (repository, dataSource) = makeSUT()
         let bookId: String = UUID().uuidString
         let book: Book = anyBook(withId: bookId)
         
@@ -125,77 +138,133 @@ final class BooksRepositoryTests: XCTestCase {
             }
             testExpectation.fulfill()
         }
-        
+        let response = true
+        dataSource.performSuccessUpdateBookCompletion(byIndex: 0, completed: response)
         wait(for: [testExpectation], timeout: 1.0)
         
         XCTAssertNotNil(didUpdate)
-        XCTAssertEqual(repository.receivedRequests, [.updateBook(bookId: bookId, book: book)])
+        XCTAssertEqual(didUpdate, response)
+        XCTAssertEqual(dataSource.receivedRequests, [.updateBook(bookId: bookId, book: book)])
     }
     
     // MARK - helpers
-    func createSpyRepositorySpy() -> BooksRepositorySpy {
-        let remoteBookDataSource = BooksRemoteDataSourceStub()
-        let repository = BooksRepositorySpy(remoteBookDataSource: remoteBookDataSource)
-        trackForMemoryLeaks(remoteBookDataSource)
+    private final class BooksRemoteDataSourceSpy: BooksRemoteDataSource {
+        private var booksStored: [String: Book] = [:]
+        
+        private(set) var receivedRequests: [ReceivedRequest] = []
+        enum ReceivedRequest: Equatable {
+            static func == (lhs: ReceivedRequest, rhs: ReceivedRequest) -> Bool {
+                switch (lhs, rhs) {
+                case (.getBooks, .getBooks):
+                    return true
+                case (.getBook(let bookIdLhs), .getBook(let bookIdRhs)):
+                    return bookIdLhs == bookIdRhs
+                case (.createBook(let bookLhs), .createBook(let bookRhs)):
+                    return (bookLhs.id == bookRhs.id) && (bookLhs.title == bookRhs.title) && (bookLhs.author == bookRhs.author)
+                case (.deleteBook(let bookIdLhs), .deleteBook(let bookIdRhs)):
+                    return bookIdLhs == bookIdRhs
+                case (.updateBook(let bookIdLhs, let bookLhs), .updateBook(let bookIdRhs, let bookRhs)):
+                    return bookIdLhs == bookIdRhs && ((bookLhs.id == bookRhs.id) && (bookLhs.title == bookRhs.title) && (bookLhs.author == bookRhs.author))
+                default:
+                    return false
+                }
+            }
+            
+            case getBooks
+            case getBook(bookId: String)
+            case createBook(book: Book)
+            case deleteBook(bookId: String)
+            case updateBook(bookId: String, book: Book)
+        }
+        
+        private var getBooksCompletions: [(RemoteResult<[Book]>) -> Void] = []
+        private var getBookCompletions: [(RemoteResult<Book>) -> Void] = []
+        private var createBookCompletions: [(RemoteResult<String>) -> Void] = []
+        private var deleteBookCompletions: [(RemoteResult<Bool>) -> Void] = []
+        private var updateBookCompletions: [(RemoteResult<Bool>) -> Void] = []
+        
+        func getBooks(completion: @escaping (ApolloGraphQLSample.RemoteResult<[ApolloGraphQLSample.Book]>) -> Void) {
+            receivedRequests.append(.getBooks)
+            getBooksCompletions.append(completion)
+        }
+        
+        func getBook(bookId: String, completion: @escaping (ApolloGraphQLSample.RemoteResult<ApolloGraphQLSample.Book>) -> Void) {
+            receivedRequests.append(.getBook(bookId: bookId))
+            getBookCompletions.append(completion)
+        }
+        
+        func createBook(book: ApolloGraphQLSample.Book, completion: @escaping (ApolloGraphQLSample.RemoteResult<String>) -> Void) {
+            receivedRequests.append(.createBook(book: book))
+            createBookCompletions.append(completion)
+        }
+        
+        func deleteBook(bookId: String, completion: @escaping (ApolloGraphQLSample.RemoteResult<Bool>) -> Void) {
+            receivedRequests.append(.deleteBook(bookId: bookId))
+            deleteBookCompletions.append(completion)
+        }
+        
+        func updateBook(bookId: String, book: ApolloGraphQLSample.Book, completion: @escaping (ApolloGraphQLSample.RemoteResult<Bool>) -> Void) {
+            receivedRequests.append(.updateBook(bookId: bookId, book: book))
+            updateBookCompletions.append(completion)
+        }
+        
+        func performSuccessGetBooksCompletion(byIndex index: Int, books: [Book]) {
+            getBooksCompletions[index](.success(books))
+        }
+        
+        func performFailureGetBooksCompletion(byIndex index: Int, error: RemoteErrors) {
+            getBooksCompletions[index](.failure(error))
+        }
+        
+        func performSuccessGetBookCompletion(byIndex index: Int, book: Book) {
+            getBookCompletions[index](.success(book))
+        }
+        
+        func performFailureGetBookCompletion(byIndex index: Int, error: RemoteErrors) {
+            getBookCompletions[index](.failure(error))
+        }
+        
+        func performSuccessCreateBookCompletion(byIndex index: Int, bookId: String) {
+            createBookCompletions[index](.success(bookId))
+        }
+        
+        func performFailureCreateBookCompletion(byIndex index: Int, error: RemoteErrors) {
+            createBookCompletions[index](.failure(error))
+        }
+        
+        func performSuccessDeleteBookCompletion(byIndex index: Int, completed: Bool) {
+            deleteBookCompletions[index](.success(completed))
+        }
+        
+        func performFailureDeleteBookCompletion(byIndex index: Int, error: RemoteErrors) {
+            deleteBookCompletions[index](.failure(error))
+        }
+        
+        func performSuccessUpdateBookCompletion(byIndex index: Int, completed: Bool) {
+            updateBookCompletions[index](.success(completed))
+        }
+        
+        func performFailureUpdateBookCompletion(byIndex index: Int, error: RemoteErrors) {
+            updateBookCompletions[index](.failure(error))
+        }
+        
+        // Mark - helpers
+        
+        private func anyBook(withId bookId: String = UUID().uuidString) -> Book {
+            Book(id: bookId, title: "title", author: "author")
+        }
+    }
+
+    
+    private func makeSUT() -> (repository: BooksRepository, dataSource: BooksRemoteDataSourceSpy) {
+        let dataSource = BooksRemoteDataSourceSpy()
+        let repository = BooksRepository(remoteBookDataSource: dataSource)
+        trackForMemoryLeaks(dataSource)
         trackForMemoryLeaks(repository)
-        return repository
+        return (repository, dataSource)
     }
     
     func anyBook(withId bookId: String = UUID().uuidString) -> Book {
         Book(id: bookId, title: "title", author: "author")
-    }
-}
-
-class BooksRepositorySpy: BooksRepository {
-    private(set) var receivedRequests: [ReceivedRequest] = []
-    
-    enum ReceivedRequest: Equatable {
-        static func == (lhs: BooksRepositorySpy.ReceivedRequest, rhs: BooksRepositorySpy.ReceivedRequest) -> Bool {
-            switch (lhs, rhs) {
-            case (.getBooks, .getBooks):
-                return true
-            case (.getBook(let bookIdLhs), .getBook(let bookIdRhs)):
-                return bookIdLhs == bookIdRhs
-            case (.createBook(let bookLhs), .createBook(let bookRhs)):
-                return (bookLhs.id == bookRhs.id) && (bookLhs.title == bookRhs.title) && (bookLhs.author == bookRhs.author)
-            case (.deleteBook(let bookIdLhs), .deleteBook(let bookIdRhs)):
-                return bookIdLhs == bookIdRhs
-            case (.updateBook(let bookIdLhs, let bookLhs), .updateBook(let bookIdRhs, let bookRhs)):
-                return bookIdLhs == bookIdRhs && ((bookLhs.id == bookRhs.id) && (bookLhs.title == bookRhs.title) && (bookLhs.author == bookRhs.author))
-            default:
-                return false
-            }
-        }
-        
-        case getBooks
-        case getBook(bookId: String)
-        case createBook(book: Book)
-        case deleteBook(bookId: String)
-        case updateBook(bookId: String, book: Book)
-    }
-    
-    override func getBooksFromRemote(completion: @escaping (RemoteResult<[Book]>) -> Void) {
-        receivedRequests.append(.getBooks)
-        super.getBooksFromRemote(completion: completion)
-    }
-    
-    override func getBookFromRemote(bookId: String, completion: @escaping (RemoteResult<Book>) -> Void) {
-        receivedRequests.append(.getBook(bookId: bookId))
-        super.getBookFromRemote(bookId: bookId, completion: completion)
-    }
-    
-    override func createBookFromRemote(book: Book, completion: @escaping (RemoteResult<String>) -> Void) {
-        receivedRequests.append(.createBook(book: book))
-        super.createBookFromRemote(book: book, completion: completion)
-    }
-    
-    override func deleteBookFromRemote(bookId: String, completion: @escaping (RemoteResult<Bool>) -> Void) {
-        receivedRequests.append(.deleteBook(bookId: bookId))
-        super.deleteBookFromRemote(bookId: bookId, completion: completion)
-    }
-    
-    override func updateBookFromRemote(bookId: String, book: Book, completion: @escaping (RemoteResult<Bool>) -> Void) {
-        receivedRequests.append(.updateBook(bookId: bookId, book: book))
-        super.updateBookFromRemote(bookId: bookId, book: book, completion: completion)
     }
 }
