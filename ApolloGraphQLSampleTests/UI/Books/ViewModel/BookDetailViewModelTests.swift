d0dae9aebf4168c573cdb8516c477e3df89a0072
IramML <irammartinezlamas@gmail.com>
2022-10-21 17:32:05 -0500

    Add tests for 'BookDetailViewModel'

--- ApolloGraphQLSampleTests/UI/Books/ViewModel/BookDetailViewModelTests.swift
+++ ApolloGraphQLSampleTests/UI/Books/ViewModel/BookDetailViewModelTests.swift
@@ -6,30 +6,56 @@
 //
 
 import XCTest
+@testable import ApolloGraphQLSample
 
 final class BookDetailViewModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_fetchBook() {
+        let (dataSource, viewModel) = makeSUT()
+        
+        var receivedValue: Book? = nil
+        let exp = expectation(description: "Wait for fetching books")
+        
+        let cancelable = viewModel.$book
+            .dropFirst()
+            .sink { value in
+                receivedValue = value
+                exp.fulfill()
+            }
+        let bookResponse = anyBook()
+        viewModel.fetchBook(byId: bookResponse.id)
+        dataSource.performSuccessGetBookCompletion(byIndex: 0, book: bookResponse)
+        
+        wait(for: [exp], timeout: 2.0)
+        
+        cancelable.cancel()
+        
+        XCTAssertNotNil(receivedValue)
+        XCTAssertEqual(receivedValue?.id, bookResponse.id)
+        XCTAssertEqual(receivedValue?.title, bookResponse.title)
+        XCTAssertEqual(receivedValue?.author, bookResponse.author)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func makeSUT() -> (dataSource: BooksRemoteDataSourceSpy, viewModel: BookDetailViewModel) {
+        let booksRemoteDataSource: BooksRemoteDataSourceSpy = BooksRemoteDataSourceSpy()
+        
+        let booksRepository: BooksRepository = BooksRepository(remoteBookDataSource: booksRemoteDataSource)
+        
+        let getBookUseCase: GetBookUseCase = GetBookUseCase(booksRepository: booksRepository)
+        
+        let viewModel = BookDetailViewModel(getBookUseCase: getBookUseCase)
+        
+        trackForMemoryLeaks(booksRemoteDataSource)
+        trackForMemoryLeaks(booksRepository)
+        trackForMemoryLeaks(getBookUseCase)
+        trackForMemoryLeaks(viewModel)
+        return (booksRemoteDataSource, viewModel)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    // MARK - helpers
+    
+    func anyBook(withId bookId: String = UUID().uuidString) -> Book {
+        Book(id: bookId, title: "title", author: "author")
     }
 
 }
